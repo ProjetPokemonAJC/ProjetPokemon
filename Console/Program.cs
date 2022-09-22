581f7b84651acb5d9c9a6c76e9a2ace8561f8ef8
Arknali Ehdon <paspons@gmail.com>
2022-09-22 15:52:39 +0200

    Revert "ménage"
    This reverts commit 117c249ceb90959695da0ee4cb8b57d6f9bd5cf3.

---
+++ Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using ConsolePokemon;
+using System.Text;
+
+internal class Programm
+{
+    // Main de l'application pokédex
+
+    static void Main(String[] args)
+    {
+
+        bool exit = false;
+        while (!exit)
+        {
+            Console.Write("\nApplication pokédex - Menu principal");
+            Console.Write("\nCréer une page pour un seul pokémon : 1");
+            Console.Write("\nIndiquer une liste de pokémon à afficher : 2");
+            Console.Write("\nFaire une recherche sur le site pokemontruc : 3");
+            Console.Write("\nQuitter : 0");
+            Console.Write("\nEntrez une commande :");
+            Console.WriteLine();
+
+
+            string param = Console.ReadLine() ?? "";
+
+            switch (param)
+            {
+                case "1":
+                    ConsolePokemon.HTML_Handler.CreerHTML();
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                case "0":
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("\nCommande non reconnnue");
+                    break;
+            }
+        }
+    }
+}
---
+++ Console/README.txt
@@ -0,0 +1,1 @@
+Projet pokémon en méthode agile
